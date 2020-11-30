9a936768bfe7672072e7b6d629373af912cbed8c
KayKay-git <kayalew@yahoo.com>
2020-11-30 13:47:41 -0500

    Oauth completed, with restrictions on users and works index and show pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login, only: [:index, :show]
+
 
   def render_404
     return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
@@ -12,6 +14,13 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_login
+    if session[:user_id].nil?
+      flash[:error] = "You must be logged in to view this section"
+      redirect_to root_path
     end
   end
 end
